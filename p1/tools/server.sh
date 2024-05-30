e0ca737ded236693e5ce72dae946455e76d0648b
msaouab <msaouab@student.1337.ma>
2024-05-30 15:14:33 +0100

    feat: changing the methods work in p1

--- p1/tools/server.sh
+++ p1/tools/server.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 sudo apk update
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
 curl -sfL https://get.k3s.io |  K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface=eth1" sh -
-./kubectl version --client
-
-echo "[CONSOLE] Waiting for k3s to start"
-sleep 5
-sudo chmod +x /var/lib/rancher/k3s/server/
-sudo cp /var/lib/rancher/k3s/server/node-token /var/www/
+sleep 10
+sudo cp /var/lib/rancher/k3s/server/node-token /home/vagrant/token
+sudo chmod 777 /home/vagrant/token
--- p1/tools/worker.sh
+++ p1/tools/worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apk update
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token `cat /var/www/node-token` --node-ip 192.168.56.111" sh -
+chmod 400 private_key
+sudo scp -q -o StrictHostKeyChecking=no -i private_key vagrant@192.168.56.110:/home/vagrant/token .
+curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_URL=https://192.168.56.110:6443/ K3S_TOKEN=`cat token` sh -
